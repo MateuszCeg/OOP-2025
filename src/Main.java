1dc68ebc3b4e667a02f5b90b9d7b491f87967a02
MateuszCeg <mateuszceg@onet.pl>
2025-02-25 18:11:14 +0100

    Zad.1 Lab1

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,11 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-
-
-
 public class Main {
     public static void main(String[] args) {
 
         Point pkt1 = new Point();
         pkt1.x = 2.2;
         pkt1.y = 3.5;
+        System.out.println(pkt1);
 
-        System.out.println("X: " + pkt1.x + "\n");
-        System.out.println("Y: " + pkt1.y);
+
     }
 }
