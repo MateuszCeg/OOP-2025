091b2cfaddb92e0f7018723884cc69dd03ca18a1
MateuszCeg <mateuszceg@onet.pl>
2025-02-25 18:58:13 +0100

    Zad.4 Lab1

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,11 @@ public class Main {
     public static void main(String[] args) {
 
         Point pkt1 = new Point();
-        pkt1.x = 2.2;
-        pkt1.y = 3.5;
+        pkt1.x = 3;
+        pkt1.y = 6;
+        System.out.println(pkt1+"\n");
+        pkt1.translate(8,11);
         System.out.println(pkt1);
-
 
     }
 }
--- src/Point.java
+++ src/Point.java
@@ -5,4 +5,21 @@ public class Point {
         return "X: "+x+" Y: "+y;
     }
 
+    public String toSvg(){
+        return " <svg height=\"1000\" width=\"500\" xmlns=\"http://www.w3.org/2000/svg\">\n" +
+                "  <circle r=\"250\" cx=\"" + x + "\" cy=\"" + y + "\" fill=\"blue\" />\n" +
+                "</svg> ";
+    }
+
+    public void translate(double dx, double dy){
+        x+=dx;
+        y+=dy;
+    }
+    public Point translated(double dx, double dy){
+        Point tr = new Point();
+        tr.x=x+dx;
+        tr.y=y+dy;
+        return tr;
+    }
+
 }
