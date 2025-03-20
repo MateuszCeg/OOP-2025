d7cb3dfe53f6fb9384248d10b3d2d9eb22de86cf
a <a@onet.pl>
2025-03-20 17:15:03 +0100

    K4 20.03

--- src/TransformationDecorator.java
+++ src/TransformationDecorator.java
@@ -13,18 +13,33 @@ public class TransformationDecorator extends ShapeDecorator{
         private float ang;
         private Vec2 angcent;
         private Vec2 scale;
+        private boolean translating= false,rotating= false,scaling= false;
 
         Builder translate(Vec2 translation){
-            transform =
+            this.transform = translation;
+            translating = true;
+            return this;
         }
         Builder rotate(float angle, Vec2 center){
-
+            this.ang = angle;
+            this.angcent = center;
+            rotating = true;
+            return this;
         }
         Builder scale(Vec2 scaleFactor){
-
+            this.scale = scaleFactor;
+            scaling = true;
+            return this;
         }
         Builder build(Shape shape){
-
+            TransformationDecorator obj = new TransformationDecorator(shape);
+            obj.transformation = "";
+            if (translating){
+                obj.transformation+="translate("+ transform.x() + " " + transform.y() + ") ";
+            }
+            if (rotate()){
+                obj.transformation+="rotate("+ transform.x() + " " + transform.y() + ") ";
+            }
         }
 
     }
